87639b7bd26a2e39e8e8c0a8643133fa341223ad
Chris Nady <nady.christopher@gmail.com>
2017-09-10 15:30:56 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,7 +15,8 @@ class EmailParser
     @emails.each_with_index do |email, index|
       if email.to_s.split(" ") != nil
         @emails.delete(email)
-        @emails.push(email.split(" "))
+        @emails.push(email.split(" ")[0])
+        @emails.push(email.split(" ")[1])
       end
     end
     @emails
